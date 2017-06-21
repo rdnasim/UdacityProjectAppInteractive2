77c8c33fb81814c06d22369261ceca59738c0598
rdnasim <rdislamnasim78@gmail.com>
2017-06-22 01:39:59 +0600

    updated

--- app/src/main/java/me/rdnasim/practiceset2/MainActivity.java
+++ app/src/main/java/me/rdnasim/practiceset2/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity {
         int weekday = 5;
         int weekend = 9;
         int optimalHours = 7 * 8;
-
-        int actualHours = weekday;
-        actualHours = actualHours + weekend * 2;
+        int actualHours = weekday * 5 + weekend * 2;
         int solution = optimalHours - actualHours;
         display(solution);
     }
